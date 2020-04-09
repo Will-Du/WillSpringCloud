6fc6d38f6af402580f232430a51af1e9a1f9a63d
Will Wu <41324809+Will-Du@users.noreply.github.com>
2020-04-09 21:05:54 +0800

    重构springcloud

---
+++ cloud-common/service-demo/src/test/java/com/wcd/servicedemo/ServiceDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wcd.servicedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
